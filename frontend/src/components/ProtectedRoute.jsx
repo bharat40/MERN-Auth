b2a9bff22346974cc49287207c31f740aea18f00
bharat <bharatdhiman40@gmail.com>
2025-07-27 15:58:24 +0530

    fix

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -7,7 +7,7 @@ const ProtectedRoute = ({ children }) => {
     useEffect(() => {
         const getProtectedData = async () => {
             try {
-                const response = await axios.get('http://localhost:5000/api/protected', {
+                const response = await axios.get('https://mern-auth-x7cu.onrender.com/api/protected', {
                     withCredentials: true
                 });
                 setIsAuthenticated(true);
